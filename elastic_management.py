f8ae6fd38524efa2c921005a62b1d991f0f5ea3e
ilyashirko <ilyashirko@gmail.com>
2023-01-04 16:04:28 +0300

    add working with quantity

--- elastic_management.py
+++ elastic_management.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from datetime import datetime
+
 # STORE_ID = '46b51b1e-1b86-41db-87bc-58298ac07645'
 
 # SECRET_KEY = 'RAqsLCdNDG9yLb18TsScXiEY3LiRoevV0nr1IQ8qF0'
@@ -11,9 +13,6 @@ import json
 # BASE_URL = 'https://api.moltin.com'
 
 
-
-
-
 def get_token(client_id: str) -> str:
     access_token_url = 'https://api.moltin.com/oauth/access_token'
     access_token_data = {
@@ -22,7 +21,10 @@ def get_token(client_id: str) -> str:
     }
     response = requests.get(access_token_url, data=access_token_data)
     response.raise_for_status()
-    return response.json()['access_token']
+    token_meta = response.json()
+    print(token_meta['access_token'])
+    print(type(token_meta['access_token']))
+    return token_meta['access_token'], token_meta['expires']
 
 
 def get_product(token: str, product_id: str) -> dict:
@@ -30,12 +32,23 @@ def get_product(token: str, product_id: str) -> dict:
         "Authorization": f"Bearer {token}"
     }
     params = {
-        "include": "main_image"
+        "include": "main_image,manage_stock",
     }
     product_url = f'https://api.moltin.com/catalog/products/{product_id}'
     response = requests.get(product_url, headers=headers, params=params)
     response.raise_for_status()
     return response.json()
+
+def get_product_stock(token: str, product_id: str) -> dict:
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    url = f'https://api.moltin.com/v2/inventories/{product_id}'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    print(json.dumps(response.json(), indent=6))
+    return response.json()
+
 
 def get_product_main_photo(token, product_id):
     headers = {
@@ -45,7 +58,6 @@ def get_product_main_photo(token, product_id):
     print(main_photo_url)
     response = requests.get(main_photo_url, headers=headers)
     response.raise_for_status()
-    input(type(response.content))
     return response.content
 
 def get_products(token: str) -> dict:
@@ -98,37 +110,52 @@ def get_customer_token(token: str,
     }
     response = requests.post(url, headers=headers, json=json_data)
     response.raise_for_status()
+    
     return response.json()['data']['token']
 
+def get_cart(token: str,
+             cart_id: str) -> dict:
+    url = f'https://api.moltin.com/v2/carts/{cart_id}'
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    params = {
+        "include": "items",
+    }
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def convert_api_date_to_timestamp(source_date):
+    datetime_obj = datetime.fromisoformat(source_date + '+03:00')
+    return int(datetime_obj.timestamp())
 
 def create_cart(token: str,
-                customer_token: str,
-                name: str = 'new_cart',
-                description: str = 'description of new cart') -> str:
+                name: str = 'new_cart') -> tuple:
     url = 'https://api.moltin.com/v2/carts'
     headers = {
         "Authorization": f"Bearer {token}",
         'Content-Type': 'application/json',
-        "x-moltin-customer-token": customer_token
     }
     json_data = {
         "data": {
             "name": name,
-            "description": description,
         }
     }
     response = requests.post(url, headers=headers, json=json_data)
     response.raise_for_status()
-    return response.json()['data']['id']
+    cart_summary = response.json()['data']
+    expired_at = cart_summary['meta']['timestamps']['expires_at']
+    return cart_summary['id'], convert_api_date_to_timestamp(expired_at)
 
 
 
 
 def add_product_to_cart(token: str,
-                        customer_token: str,
                         prod_id: str,
-                        reference: str,
-                        cart_id: str):
+                        cart_id: str,
+                        quantity: int):
     url = f'https://api.moltin.com/v2/carts/{cart_id}/items'
     headers = {
         "Authorization": f"Bearer {token}",
@@ -138,10 +165,11 @@ def add_product_to_cart(token: str,
         "data": {
             "id": prod_id,
             "type": 'cart_item',
-            'quantity': 1
+            'quantity': quantity
         }
     }
     response = requests.post(url, headers=headers, json=json_data)
+    
     response.raise_for_status()
     return response.json()
 
@@ -158,13 +186,19 @@ if __name__ == '__main__':
 
 
     token = get_token(CLIENT_ID)
-    print(token)
+    #token = 'fa5ce5be-62ea-4f72-8550-20ce6587b603'
+    # print(json.dumps(token, indent=2))
 
-    products = get_products(token)
-
+    # input(json.dumps(create_cart(token, '23234234'), indent=2))
+    input(create_cart(token, 'blabla'))
+    try:
+        products = get_products(token)
+    except requests.exceptions.HTTPError as error:
+        input(error.response.status_code)
+    CART_ID = create_cart(token, 'blabla')
     prod_id = products['data'][1]['id']
-
-    input(get_product_main_photo(token, prod_id))
+    add_product_to_cart(token, prod_id, CART_ID, 5)
+    input(json.dumps(get_cart(token, CART_ID), indent=2))
 
 
     input()
--- telegram_bot.py
+++ telegram_bot.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from inspect import Attribute
+from multiprocessing.sharedctypes import Value
 from xmlrpc.client import ResponseError
 from telegram import (
     Update,
@@ -29,14 +32,176 @@ DEFAULT_REDIS_PASSWORD = None
 
 DEFAULT_REDIS_DB = 0
 
-RETURN_INLINE_KEYBOARD = InlineKeyboardMarkup(
-    [[InlineKeyboardButton(text='Назад', callback_data='main_menu')]]
-)
+SPARE_TOKEN_CART_TIME = 300
+# SPARE_TOKEN_CART_TIME is used to prevent token or cart death while current script processing.
+# 300 - random value ensuring that there is enough time for script processing
+
+
+class ExpirationError(Exception):
+    pass
+
+
+class EntityNotFound(Exception):
+    pass
+
+
+def get_or_create_elastic_token(redis: Redis) -> str:
+    try:
+        token = redis.get('ELASTIC_AUTH_TOKEN')
+        if not token:
+            raise EntityNotFound
+
+        expired_at = int(redis.get('ELASTIC_AUTH_TOKEN_expires').decode('utf-8'))
+        if datetime.now().timestamp() + SPARE_TOKEN_CART_TIME > expired_at:
+            raise ExpirationError
+
+        return token.decode('utf-8')
+
+    except (EntityNotFound, ExpirationError):
+        token, expired_at = elastic_management.get_token(env.str('CLIENT_ID'))
+        
+        redis.set('ELASTIC_AUTH_TOKEN', token)
+        redis.set('ELASTIC_AUTH_TOKEN_expires', expired_at)
+        
+        return token
+
+
+def get_or_create_cart(redis: Redis, user_tg_id: int, elastic_token: str) -> tuple[str, bool]:
+    try:
+        user_cart_id = redis.get(f'{user_tg_id}_cart_id')
+        if not user_cart_id:
+            raise EntityNotFound
+        expired_at = redis.get(f'{user_tg_id}_cart_expires')
+        if not expired_at:
+            # it can be if redis lose "expired" record with actual cart.
+            # captured while testing
+            raise EntityNotFound
+        if datetime.now().timestamp() + 300 > int(expired_at.decode('utf-8')):
+            raise ExpirationError
+        
+        return user_cart_id.decode('utf-8'), False
+
+    except (EntityNotFound, ExpirationError):
+        user_cart_id, expired_at = elastic_management.create_cart(elastic_token, str(user_tg_id))
+        
+        redis.set(f'{user_tg_id}_cart_id', user_cart_id)
+        redis.set(f'{user_tg_id}_cart_expires', expired_at)
+
+        return user_cart_id, True
+
+
+def get_current_quantity_in_cart(redis: Redis, user_tg_id: int, elastic_token: str, product_id) -> int:
+    user_cart_id, is_empty = get_or_create_cart(redis, user_tg_id, elastic_token)
+    if is_empty:
+        return 0
+    user_cart = elastic_management.get_cart(elastic_token, user_cart_id)
+    for item in user_cart['included']['items']:
+        if item['product_id'] == product_id:
+            return item['quantity']
+    return 0
+    
+
+def make_prod_inline(quantity: int = 1, product_id: str = 'null'):
+    return InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text='-', callback_data=f'reduce_quantity:{product_id}'),
+                InlineKeyboardButton(text=f'{quantity}', callback_data=f'add_to_cart:{product_id}'),
+                InlineKeyboardButton(text='+', callback_data=f'increase_quantity:{product_id}')
+            ],
+            [InlineKeyboardButton(text='Добавить в корзину', callback_data=f'add_to_cart:{product_id}')],
+            [InlineKeyboardButton(text='Назад', callback_data='main_menu')]
+        ]
+    )
+
+def increase_quantity(redis: Redis,
+                      update: Update,
+                      context: CallbackContext) -> str:
+    _, product_id = update.callback_query.data.split(':')
+    elastic_token = get_or_create_elastic_token(redis)
+
+    product_stock = elastic_management.get_product_stock(elastic_token, product_id)
+    available_pcs = product_stock['data']['available']
+
+    quantity_in_cart = get_current_quantity_in_cart(redis, update.effective_chat.id, elastic_token, product_id)
+    current_quantity = int(update.callback_query.message.reply_markup['inline_keyboard'][0][1]['text'])
+
+
+    if current_quantity + quantity_in_cart < available_pcs:
+        context.bot.edit_message_reply_markup(
+            chat_id=update.effective_chat.id,
+            message_id=update.callback_query.message.message_id,
+            reply_markup=make_prod_inline(quantity=(current_quantity + 1), product_id=product_id)
+        )
+    else:
+        message = str()
+
+        if quantity_in_cart:
+            message += f'You already added {quantity_in_cart} pcs to you cart.\n\n'
+
+        message += f'You can purchase only {available_pcs} psc.'
+
+        context.bot.send_message(
+            update.effective_chat.id,
+            message
+        )
+    return 'HANDLE_DESCRIPTION'
+
+
+def reduce_quantity(update: Update,
+                    context: CallbackContext) -> str:
+    _, product_id = update.callback_query.data.split(':')
+    current_quantity = int(update.callback_query.message.reply_markup['inline_keyboard'][0][1]['text'])
+    if current_quantity > 1:
+        context.bot.edit_message_reply_markup(
+            chat_id=update.effective_chat.id,
+            message_id=update.callback_query.message.message_id,
+            reply_markup=make_prod_inline(quantity=(current_quantity - 1), product_id=product_id)
+        )
+    return 'HANDLE_DESCRIPTION'
+
+
+def add_to_cart(redis: Redis,
+                update: Update,
+                context: CallbackContext) -> str:
+
+    _, product_id = update.callback_query.data.split(':')
+    quantity = int(update.callback_query.message.reply_markup['inline_keyboard'][0][1]['text'])
+    
+    elastic_token = get_or_create_elastic_token(redis)
+    
+    user_cart_id, is_empty = get_or_create_cart(redis, update.effective_chat.id, elastic_token)
+    
+    try:
+        elastic_management.add_product_to_cart(elastic_token, product_id, user_cart_id, quantity)
+    except requests.exceptions.HTTPError as error:
+        if error.response.status_code == 400:
+            product_stock = elastic_management.get_product_stock(elastic_token, product_id)
+            available_pcs = product_stock['data']['available']
+            quantity_in_cart = get_current_quantity_in_cart(redis, update.effective_chat.id, elastic_token, product_id)
+            context.bot.send_message(
+                update.effective_chat.id,
+                dedent(
+                    f'''
+                    Sorry, you are trying to add too much pcs.
+
+                    Now in your cart: {quantity_in_cart} pcs.
+                    Available for order: {available_pcs} pcs.
+                    '''
+                )
+            )
+        else:
+            context.bot.send_message(
+                update.effective_chat.id,
+                'Sorry, cant add this good to your cart.'
+            )
+    
+    return 'HANDLE_DESCRIPTION'
 
 def main_menu(update: Update,
               context: CallbackContext) -> str:
-    token = elastic_management.get_token(env.str('CLIENT_ID'))
-    products = elastic_management.get_products(token)
+    elastic_token = get_or_create_elastic_token(redis)
+    products = elastic_management.get_products(elastic_token)
     if update.callback_query:
         context.bot.delete_message(
             chat_id=update.effective_chat.id,
@@ -63,8 +228,10 @@ def main_menu(update: Update,
 def handle_menu(update: Update,
                 context: CallbackContext) -> str:
     _, product_id = update.callback_query.data.split(':')
-    token = elastic_management.get_token(env.str('CLIENT_ID'))
-    product = elastic_management.get_product(token, product_id)
+    elastic_token = get_or_create_elastic_token(redis)
+    product = elastic_management.get_product(elastic_token, product_id)
+    product_stock = elastic_management.get_product_stock(elastic_token, product_id)
+
     response = requests.get(product['included']['main_images'][0]['link']['href'])
     context.bot.send_photo(
         chat_id=update.effective_chat.id,
@@ -76,9 +243,11 @@ def handle_menu(update: Update,
             {product['data']['attributes']['description']}
 
             {product['data']['attributes']['price']['USD']['amount'] / 100} USD
+
+            {product_stock['data']['available']} psc available
             """
         ),
-        reply_markup=RETURN_INLINE_KEYBOARD
+        reply_markup=make_prod_inline(product_id=product_id)
     )
     context.bot.delete_message(
         chat_id=update.effective_chat.id,
@@ -111,7 +280,10 @@ if __name__ == '__main__':
                     CallbackQueryHandler(callback=handle_menu, pattern='product')
                 ],
                 'HANDLE_DESCRIPTION': [
-                    CallbackQueryHandler(callback=main_menu, pattern='main_menu')
+                    CallbackQueryHandler(callback=main_menu, pattern='main_menu'),
+                    CallbackQueryHandler(callback=partial(increase_quantity, redis), pattern='increase_quantity'),
+                    CallbackQueryHandler(callback=reduce_quantity, pattern='reduce_quantity'),
+                    CallbackQueryHandler(callback=partial(add_to_cart, redis), pattern='add_to_cart')
                 ]
             },
             fallbacks=[]
